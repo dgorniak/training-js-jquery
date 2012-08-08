6429a2e603895590b473009ae9e435d0edc7f340
Dawid Gorniak <dawidgorniak@gmail.com>
2012-08-08 23:04:58 +0200

    Started work on first example

--- basics/demo.js
+++ basics/demo.js
@@ -1,1 +1,22 @@
-alert('test')
+// Przygotuj tablicę z nazwami miesięcy
+
+var months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień']
+
+//Wylistuj wszystkie miesiące
+   
+for(i=0; i<months.length; i++) {
+  document.write("<p>" + (i+1) + ". " + months[i] + "</p>");            
+}
+    
+//Wybierz miesiąc o najdłuższej nazwie
+var longestName = ''
+    
+for(i=0; i<months.length; i++) {
+    if(months[i].length > longestName.length) {
+      longestName = months[i]
+    }
+}
+
+document.write("<br/>");
+document.write("Miesiąc o najdłuższej nazwie to: " + longestName)    
+    
