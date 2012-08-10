d94b6b367facbf21bbb5292beba0b84364d90a06
Dawid Górniak <dawidgorniak@gmail.com>
2012-08-10 23:58:56 +0200

    Validated sources with jsLint

--- basics/demo.js
+++ basics/demo.js
@@ -1,48 +1,43 @@
 // Przygotuj tablicę z nazwami miesięcy
-
-var months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień']
+var months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
 
 //Wylistuj wszystkie miesiące
-   
-for(i=0; i<months.length; i++) {
-  document.write("<p>" + (i+1) + ". " + months[i] + "</p>");            
+for (i = 0; i < months.length; i++) {
+    document.write("<p>" + (i + 1) + ". " + months[i] + "</p>");
 }
-    
+
 //Wybierz miesiąc o najdłuższej nazwie
-var longestName = ''
-    
-for(i=0; i<months.length; i++) {
-    if(months[i].length > longestName.length) {
-      longestName = months[i]
+var longestName = '';
+
+for (i = 0; i < months.length; i++) {
+    if (months[i].length > longestName.length) {
+        longestName = months[i];
     }
 }
 
 document.write("<br/>");
-document.write("Miesiąc o najdłuższej nazwie to: " + longestName)
+document.write("Miesiąc o najdłuższej nazwie to: " + longestName);
 
 //Uproszczona zmiana formatu daty
- 
-var shortDateString = "2012-01-01"    
+var shortDateString = "2012-01-01";
 
-var dateArr = shortDateString.split("-")
-    
-if(dateArr.length != 3) {
+var dateArr = shortDateString.split("-");
+
+if (dateArr.length != 3) {
     alert("Niepoprawny format daty");
     return;
 }
 
 var monthIndex = parseInt(dateArr[1], 10);
 
-if(typeof monthIndex != 'number' || isNaN(monthIndex)) {
+if (isNaN(monthIndex)) {
     alert("Niepoprawny numer miesiąca");
     return;
 }
-    
-var longDateString = dateArr[2] + " " + months[monthIndex] + " " + dateArr[0]
-    
-document.write("<br/>")
+
+var longDateString = dateArr[2] + " " + months[monthIndex] + " " + dateArr[0];
+
+document.write("<br/>");
 document.write("Skrócona data: " + shortDateString);
-document.write("<br/>")
-document.write("Pełna data: " + longDateString);
-    
-​
+document.write("<br/>");
+document.write("Pełna data: " + longDateString);​
--- parseInt/demo.js
+++ parseInt/demo.js
@@ -1,14 +1,17 @@
-var ints = new Object()
+var ints = {};
+var number;
 
-ints["1"]         =  parseInt("1");    
-ints["01"]        =  parseInt("01");
-ints["09"]        =  parseInt("09");
-ints["09_10"]     =  parseInt("09", 10);
-ints["20 USD"]    =  parseInt("20 USD");
+ints["1"] = parseInt("1");
+ints["01"] = parseInt("01");
+ints["09"] = parseInt("09");
+ints["09_10"] = parseInt("09", 10);
+ints["20 USD"] = parseInt("20 USD");
 
-document.write("Parsed integers...")
+document.write("Parsed integers...");
 
-for(number in ints) {
-    document.write("<br/>")
-    document.write("\"" + number + "\"" + " : " + ints[number])
-}
+for (number in ints) {
+    if (ints.hasOwnProperty(number)) {
+        document.write("<br/>");
+        document.write("\"" + number + "\"" + " : " + ints[number]);
+    }
+}​
