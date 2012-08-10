82576031f946b3ad9aef12d0eff1970e99520884
Dawid Górniak <dawidgorniak@gmail.com>
2012-08-11 00:47:45 +0200

    Added semicolon insertion example

---
+++ semicolonInsertion/demo.js
@@ -0,0 +1,15 @@
+function fun1() {
+    return {
+        capacity: 5
+    }
+}
+
+function fun2() {
+    return {
+        capacity: 5
+    };
+}
+
+
+document.write("Fun1: " + fun1() + "<br/>");
+document.write("Fun2: " + fun2());​
