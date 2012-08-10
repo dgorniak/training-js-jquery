368a1f5c88af8ea3e7c4e24cfc0ef4fbe2a1aabd
Dawid Górniak <dawidgorniak@gmail.com>
2012-08-10 20:24:40 +0200

    Added parseInt samples.

---
+++ parseInt/demo.js
@@ -0,0 +1,14 @@
+var ints = new Object()
+
+ints["1"]         =  parseInt("1");    
+ints["01"]        =  parseInt("01");
+ints["09"]        =  parseInt("09");
+ints["09_10"]     =  parseInt("09", 10);
+ints["20 USD"]    =  parseInt("20 USD");
+
+document.write("Parsed integers...")
+
+for(number in ints) {
+    document.write("<br/>")
+    document.write("\"" + number + "\"" + " : " + ints[number])
+}
