9b115b28e30ac8fd6b352cd841136db9b9e26b52
Dawid Górniak <dawidgorniak@gmail.com>
2012-08-19 16:53:43 +0200

    Added autocomplete example

---
+++ jQueryUI/autocomplete/demo.js
@@ -0,0 +1,16 @@
+//add resource: http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/ui-lightness/jquery-ui.css
+
+var countries = [
+            "Wielka Brytania",
+            "Niemcy",
+            "Francja",
+            "Hiszpania",
+            "Polska",
+            "Włochy",
+            "Hiszpania"
+];
+$( "#country" ).autocomplete({
+   source: countries
+});
+
+​
