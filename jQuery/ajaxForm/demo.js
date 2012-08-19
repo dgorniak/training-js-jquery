8215d5546bf21fa602208aff9796b23917012a76
Dawid Górniak <dawidgorniak@gmail.com>
2012-08-19 13:01:40 +0200

    Added AJAX form demo

---
+++ jQuery/ajaxForm/demo.js
@@ -0,0 +1,68 @@
+//add resource: http://jquery-json.googlecode.com/files/jquery.json-2.2.min.js
+
+var Examples = {
+    
+    validateRegisterForm : function() {
+      var email = $("input#email").val();  
+        
+      if(email.length === 0 || (email.indexOf("@") < 0)) {
+        var errorsDiv = $("#register-form .validation-errors");
+        errorsDiv.append("<p class='error'>Adres email jest niepoprawny</p>");
+        
+        $('form#register-form label').addClass("error");
+        $('form#register-form #email').addClass("error");
+                  
+        var errorParagraph = $("#register-form .validation-errors .error");
+        errorParagraph.fadeOut(10000);                      
+          
+        return false;
+      }
+      
+      //Remove error classes if present
+      $('form#register-form label').removeClass("error");
+      $('form#register-form #email').removeClass("error");
+      
+      //Universal submit blocker        
+      $('form#register-form input[type=submit]').attr('disabled', 'disabled');
+      
+      this.sendRegisterRequest(email);
+        
+      return false;
+    },
+    sendRegisterRequest: function(email) {
+       
+        var data = {
+          json: $.toJSON({
+            html: '<p class="info">Konto użytkownika o adresie ' + email 
+                + ' zostało pomyślnie zarejestrowane</p>'
+          }),
+         delay: 2
+       };
+       
+       $.ajax({
+          url:"/echo/json/",
+          data:data,
+          type:"POST"
+       }).done(function(response){
+          $('form#register-form').get(0).reset();
+           
+          $("#statusMessages").append(response.html);
+          $('#statusMessages .info').fadeOut(10000); 
+           
+          $('form#register-form input[type=submit]').attr('disabled', '');
+           
+       }).fail(function(jqXHR, textStatus){
+          alert('Błąd połączenia: ' + textStatus);
+       });  
+   }
+};
+
+   
+//Event handlers
+
+$(document).ready(function() { 
+  $('#register-form').submit(function() {
+    return Examples.validateRegisterForm();
+  });
+});
+​
